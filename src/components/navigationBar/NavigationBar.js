47987c462fad03e4c15a8a59914bed83a326e2a8
Pedro Lima <pedrohenriquefelima@gmail.com>
2023-09-24 16:40:04 +0100

    navigation-bar-raw

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from './NavigationBar.module.css';
 import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
+    const [mobile, setMobile] = useState(false)
     return (
-        <h1>I am a navigation</h1>
+        <nav className={styles.navbar}>
+           
+                <h3 className={styles.logo}>Logo</h3>
+                <ul className={mobile ? styles['nav-links-mobile'] : styles['nav-links']} onClick={() => setMobile(false)}>
+                    <Link to='/'>
+                        <li>Home</li>
+                    </Link>
+                    <Link to='/'>
+                        <li>Download</li>
+                    </Link>
+                    <Link to='/'>
+                        <li>Community</li>
+                    </Link>
+                    <Link to='/'>
+                        <li>Features</li>
+                    </Link>
+                    <Link to='/'>
+                        <li>Login</li>
+                    </Link>
+                    <Link to='/'>
+                        <li>Sign up</li>
+                    </Link>
+                </ul>
+                <button className={styles['mobile-menu-icon']} onClick={()=>setMobile(!mobile)}>
+                    {mobile ? 'X' : 'Y'}
+                </button>
+    
+        </nav>
     )
     
 }
