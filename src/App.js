4708872787fe7aa767449c0493aeaed924dad6a4
Pedro Lima <pedrohenriquefelima@gmail.com>
2023-09-26 21:19:06 +0100

    NAVLINK

--- src/App.js
+++ src/App.js
@@ -9,17 +9,23 @@ import SignUpPage from "./pages/signUpPage/SignUpPage";
 import ErrorPage from "./pages/errorPage/ErrorPage";
 
 //every object represents one route
+//react can deal with dynamic paths
+//by adding a semicolon to the path /community/:id
+//the component receiving this route can use the useParams hook
+//the routes below use a relative path (absolute path starts with /)
+//relative vs route property on NavLink
+
 const router = createBrowserRouter([
   { path: '/', 
     element: <Root/>,
     errorElement: <ErrorPage/>,
     children: [
-    { path: '/', element: <HomePage/>},
-    { path: '/download', element: <DownloadPage/>},
-    { path: '/community', element: <CommunityPage/>},
-    { path: '/features', element: <FeaturePage/>},
-    { path: '/login', element: <LoginPage/>},
-    { path: '/signup', element: <SignUpPage/>}
+    { path: '', element: <HomePage/>},
+    { path: 'download', element: <DownloadPage/>},
+    { path: 'community', element: <CommunityPage/>},
+    { path: 'features', element: <FeaturePage/>},
+    { path: 'login', element: <LoginPage/>},
+    { path: 'signup', element: <SignUpPage/>}
   ]}
 ]);
 
--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from './NavigationBar.module.css';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import LOGO from "../../../src/materials/images/u-chat.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
@@ -11,30 +11,31 @@ library.add([faX,faBars]);
 
 const NavigationBar = () => {
     const [mobile, setMobile] = useState(false)
+
     return (
         <nav className={styles.navbar}>
             <div className={styles['image-container']}>
                 <img src={LOGO} alt="Logo" className={styles['image-logo']}/>
             </div>
             <ul className={mobile ? styles['nav-links-mobile'] : styles['nav-links']} onClick={() => setMobile(false)}>
-                <Link to='/'>
-                    <li>Home</li>
-                </Link>
-                <Link to='/download'>
-                    <li>Download</li>
-                </Link>
-                <Link to='/community'>
-                    <li>Community</li>
-                </Link>
-                <Link to='/features'>
-                    <li>Features</li>
-                </Link>
-                <Link to='/login'>
-                    <li>Login</li>
-                </Link>
-                <Link to='/signup'>
-                    <li>Sign up</li>
-                </Link>
+                <li>
+                    <NavLink to='/' className={({ isActive }) => isActive ? styles.active : undefined}>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='download' className={({ isActive }) => isActive ? styles.active : undefined}>Download</NavLink>
+                </li>
+                <li>
+                    <NavLink to='community' className={({ isActive }) => isActive ? styles.active : undefined}>Community</NavLink>
+                </li>
+                <li>
+                    <NavLink to='features' className={({ isActive }) => isActive ? styles.active : undefined}>Features</NavLink>
+                </li>
+                <li>
+                    <NavLink to='login' className={({ isActive }) => isActive ? styles.active : undefined}>Login</NavLink>
+                </li>
+                <li>
+                    <NavLink to='signup' className={({ isActive }) => isActive ? styles.active : undefined}>Sign up</NavLink>
+                </li>
             </ul>
             <button className={styles['mobile-menu-icon']} onClick={()=>setMobile(!mobile)}>
                 {mobile ? <FontAwesomeIcon icon="fa-solid fa-x" className={styles.cross} /> : <FontAwesomeIcon icon="fa-solid fa-bars" className={styles.bars}/>}
