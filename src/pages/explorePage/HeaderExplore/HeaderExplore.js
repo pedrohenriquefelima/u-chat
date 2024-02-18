587f4cc5488cb569dc26a52c9d12023ee6e4f453
Pedro Lima <pedrohenriquefelima@gmail.com>
2024-02-18 22:35:06 +0000

    header-explore

--- src/pages/explorePage/HeaderExplore/HeaderExplore.js
+++ src/pages/explorePage/HeaderExplore/HeaderExplore.js
@@ -5,7 +5,7 @@ const HeaderExplore = () => {
     return (
         <div className={styles['container-learning']}>
             <div className={styles['hero-text']}>
-                <h3 className={styles.enthusiasts}>Find your language partner online</h3>
+                <h3 className={styles.enthusiasts}>FIND YOUR LANGUAGE PARTNER <span className={styles['online-text']}>ONLINE</span></h3>
                 <p> At Uchat, we believe that learning a new language can be fun, engaging, and most importantly, effective when you have the right language partner. Whether you're a beginner looking to grasp the basics or an advanced learner aiming to refine your skills, our platform connects language enthusiasts from around the world to facilitate meaningful language exchange. </p>
             </div>
             <img src={LEARNING_IMG} alt='test' className={styles.learning}/>
--- src/pages/explorePage/UsersFilters/UsersFilters.js
+++ src/pages/explorePage/UsersFilters/UsersFilters.js
@@ -1,21 +1,106 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './UsersFilters.module.css';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const UsersFilters = () => {
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const interestOptionsValues = [
+        { name: 'Reading', popular: true },
+        { name: 'Writing', popular: true },
+        { name: 'Sleeping', popular: false },
+        { name: 'Coding', popular: true },
+        { name: 'Gaming', popular: true },
+        { name: 'Traveling', popular: true },
+        { name: 'Cooking', popular: false },
+        { name: 'Photography', popular: true },
+        { name: 'Fitness', popular: false },
+        { name: 'Painting', popular: true },
+        { name: 'Music', popular: true },
+        { name: 'Movies', popular: true },
+        { name: 'Dancing', popular: false },
+        { name: 'Learning', popular: true },
+        { name: 'Teaching', popular: false },
+        { name: 'Singing', popular: true },
+        { name: 'Hiking', popular: true },
+        { name: 'Shopping', popular: false },
+        { name: 'Yoga', popular: true },
+        { name: 'Meditation', popular: true },
+      ]
+    const [interestOptions, setInterestOptions] = useState([]);
+    const [open, setOpen] = useState(false)
+    const [selectedInterests, setSelectedInterest] = useState([]);
+    const [inputValue, setInputValue] = useState("");
 
     useEffect(()=>{
-        //when it loads for the first time loads all users
-        
+        interestOptionsValues.sort((a, b) => a.name.localeCompare(b.name));
+        //remove popula ones and put on its on grouping
+        let interestGrouped = [];
+        interestGrouped.push({Popular: interestOptionsValues.filter(item => item.popular)});
+
+        for (let step = 0; step < alphabet.length; step++) {
+            console.log(alphabet[step]);
+            const letter = alphabet[step].toLowerCase();
+            interestGrouped.push({[alphabet[step]] : interestOptionsValues.filter(item => item.name.toLowerCase().startsWith(letter) && !item.popular)});
+        }
+        console.log(JSON.stringify(interestGrouped));
+        //remove all groups with empty arrays
+       
+        setInterestOptions( interestGrouped.filter(item => Object.values(item)[0].length > 0));
     },[])
+
+    function handleItemClick(objItem) {
+        const exists = selectedInterests?.some(item => item.name === objItem.name);
+        if(exists) {
+            const selectedInteTemp = selectedInterests?.filter(item => item.name != objItem.name);
+            console.log('selectedInteTemp:::', JSON.stringify(selectedInteTemp));
+            setSelectedInterest(selectedInteTemp);
+            return;
+        }
+        console.log('selectedInterests::::',JSON.stringify(selectedInterests));
+        setSelectedInterest((current) => {
+            return [...current, objItem];
+        })
+    }
 
     return (
         <div className={styles['filtering-container']}>
-           <div className={styles.button}>Interests</div>
-           <div className={styles.button}>Learning</div>
-           <div className={styles.button}>Speaks</div>
-           <div className={styles.button}>Level</div>
-           <div className={styles.clear}>Clear</div>
-           <div className={styles.search}></div>
+            <div className={styles['main-filter']}>
+                <div>
+                <div className={styles.button} onClick={()=>setOpen(!open)}>Interests</div>
+                    {open &&<ul className={styles['dropdown-ul']}>
+                            <div className={styles.inputContainer}>
+                                <FontAwesomeIcon icon={faSearch}  className={styles.icon} />
+                                <input className={styles['type-here']} type='text' placeholder='Type here...' onChange={(e)=>setInputValue(e.target.value)} value={inputValue}/>
+                            </div>
+                            {selectedInterests?.length > 0 &&<div>
+                                {JSON.stringify(selectedInterests)}
+                            </div>}
+                       {
+                            interestOptions?.map(option => {
+                                const propertyNames = Object.keys(option);
+                                const arrayValue = option[propertyNames[0]];
+                                console.log('propertyNames: ', propertyNames, 'arrayValue: ', JSON.stringify(arrayValue));
+                                console.log(inputValue);
+                                // const shouldHide = arrayValue.some(value => value.name !== inputValue);
+                                
+                                return (
+                                        <div>
+                                            <span className={[`${styles['group-title']} ${propertyNames[0].toLowerCase().startsWith(inputValue) ||  inputValue === ""? styles['item-show'] : styles['item-hide']}`]}>{propertyNames}</span>
+                                            {arrayValue?.map(item => {
+                                                return <li className={[`${styles['option-item']} ${selectedInterests?.some(inter => inter.name === item.name) ? styles['item-selected']  : styles['option-item-hover']} ${item?.name?.toLowerCase().startsWith(inputValue) ? styles['item-show'] : styles['item-hide']}`]} onClick={() => handleItemClick(item)}>{item.name}</li>
+                                            })}
+                                        </div>)
+                            })
+                       }
+                    </ul>}
+                </div>
+                
+                <div className={styles.button}>Learning</div>
+                <div className={styles.button}>Teaching</div>
+                <div className={styles.button}>Clear</div>
+            </div>
+           
         </div>
     )
 }
